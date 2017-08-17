c09f2f88433001f0796b6ca6236802651b2cea27
yrubin <yahav7rubin@gmail.com>
2017-08-17 19:44:25 +0300

    json for links starting telegram bot

--- scraper.py
+++ scraper.py
@@ -3,19 +3,26 @@ from scrapy.crawler import CrawlerProcess
 
 class NasSpider(scrapy.Spider):
     name = "nas_spider"
-    start_urls = ['http://www.nasdaq.com/symbol/aapl/news-headlines?page=1']
+    start_urls = ['http://www.nasdaq.com/symbol/ibm/news-headlines?page=1']
 
     def parse(self, response):
-        SET_SELECTOR = '.fontS14px'
-        for span in response.css(SET_SELECTOR):
+        link = []
+        headerSelector='.news-headlines'
+        for div in response.css(headerSelector):
+            SET_SELECTOR = '.fontS14px'
+            for span in response.css(SET_SELECTOR):
+                NAME_SELECTOR = 'a'
+                IMAGE_SELECTOR = 'a ::attr(href)'
+                yield {
+                  #  'name' : span.css(NAME_SELECTOR).extract_first(),
+                    'link' : span.css(IMAGE_SELECTOR).extract_first(),
+                }
 
-            NAME_SELECTOR = 'a'
-            IMAGE_SELECTOR = 'a ::attr(href)'
-            yield  {
-              #  'name' : span.css(NAME_SELECTOR).extract_first(),
-                'link' : span.css(IMAGE_SELECTOR).extract_first(),
-            }
 
-process = CrawlerProcess({})
+process = CrawlerProcess({
+'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
+'FEED_FORMAT': 'json',
+'FEED_URI': 'result.json'
+})
 process.crawl(NasSpider)
 process.start()
