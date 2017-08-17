a8886bd3d9a59f0878cf5d3f70b10ca5a9924d59
yrubin <yahav7rubin@gmail.com>
2017-08-17 21:42:02 +0300

    added part1 of tests

--- article_processor/testing.py
+++ article_processor/testing.py
@@ -3,6 +3,19 @@ from article_processor import Processor
 
 test_set = [
     (r"http://www.nasdaq.com/article/the-3-top-stocks-for-your-ira-in-august-cm830110", 1)
+    (r"http://www.nasdaq.com/article/verizon-vz-and-amazon-team-up-for-virtual-network-services-cm833256",0)
+    (r"http://www.nasdaq.com/article/warren-buffetts-had-it-with-these-stocks-cm833124",0)
+    (r"http://www.nasdaq.com/article/2-high-yield-warren-buffett-stocks-and-1-he-wishes-he-could-buy-cm832975",1)
+    (r"http://www.nasdaq.com/article/the-2-safest-dividend-stocks-to-buy-in-tech-cm832258",1)
+    (r"http://www.nasdaq.com/article/the-zacks-analyst-blog-highlights-intel-microsoft-cisco-ibm-and-nvidia-cm832306",-1)
+    (r"http://www.nasdaq.com/article/microsoft-unveils-enterprise-blockchain-coco-framework-cm832313",1)
+    (r"http://www.nasdaq.com/article/tableau-acquires-cleargraph-enables-data-discovery-via-voice-cm832233",-1)
+    (r"http://www.nasdaq.com/article/3-stocks-that-look-just-like-activision-blizzard-in-2013-cm832133",1)
+    (r"http://www.nasdaq.com/article/should-you-invest-in-intel-intc-now-cm831870",-1)
+    (r"https://seekingalpha.com/article/4099518-cogint-second-largest-holding?source=nasdaq",1)
+    (r"http://www.investopedia.com/news/warren-buffett-enters-synchrony-financial-adds-bank-new-york-mellon-13f-berkshire-hathaway/?utm_campaign=quote-nasdaq&utm_source=nasdaq&utm_medium=referral&utm_term=fb-capture&utm_content=/#ec|rss-nasdaq",0)
+
+
  ]
 
 
