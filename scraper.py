efc0a5a0d77f4da94dd184839cadf849acb24ba2
yrubin <yahav7rubin@gmail.com>
2017-08-17 20:06:17 +0300

    linking article processor and scraper

--- scraper.py
+++ scraper.py
@@ -19,10 +19,3 @@ class NasSpider(scrapy.Spider):
                 }
 
 
-process = CrawlerProcess({
-'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
-'FEED_FORMAT': 'json',
-'FEED_URI': 'result.json'
-})
-process.crawl(NasSpider)
-process.start()
