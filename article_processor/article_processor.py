d0dbe816e33f78c1407626c56d4b4b46222713c9
gefenk <gefenk9@gmail.com>
2017-08-17 21:45:36 +0300

    update algo decision

--- article_processor/article_processor.py
+++ article_processor/article_processor.py
@@ -11,6 +11,39 @@ class Processor:
                 counter += 1
         return counter
 
+    def _decide(self, pos_point, neg_point, nlp_decision, nlp_confidence):
+        """
+
+        :param pos_point:
+        :param neg_point:
+        :param nlp_decision:
+        :param nlp_confidence:
+        :return:
+        """
+        if pos_point is 0 and neg_point is 0:
+            if nlp_confidence > 0.95 and nlp_decision is 1:
+                return (1, nlp_confidence)
+            else:
+                return (-1, 1)
+        else:
+            # if there is points
+
+            # absolut accourance of type of words
+            if pos_point > 2*neg_point:
+                return (1, pos_point/(pos_point + neg_point))
+            if neg_point > pos_point:
+                return (0, neg_point/(pos_point + neg_point))
+
+            if pos_point > neg_point:
+                if nlp_decision is 1:
+                    return (1, nlp_confidence)
+                else:
+                    return (-1, 1)
+            else:
+                # more negative:
+                return (0, 1)
+
+
     def process(self, url = ""):
         """
 
@@ -18,8 +51,10 @@ class Processor:
         :return:
         """
         pos = ["grow", "potential", "growth", "economic necessity", "increased demand", "growing", "evolving",
-               "major growth", "impressive", "good", "rise", "top stocks for you", "high-yielding"]
-        neg = ["down", "lower", "disappointing", "disappoint", "wrong", "disappointment", "downside", "risk", "struggling", "spending"]
+               "major growth", "impressive", "good", "rise", "top stocks for you", "high-yielding",
+               "bullish", "attractive", "growth"]
+        neg = ["down", "lower", "disappointing", "disappoint", "wrong", "disappointment", "downside", "risk",
+               "struggling", "spending", "berish", "falling", "fails", "bearish", "risky", "dropping"]
         pos_point = 0
         neg_point = 0
         try:
@@ -40,13 +75,9 @@ class Processor:
             else:
                 return (sentiment['polarity'] == 'positive', sentiment['polarity_confidence'])
 
+            return self._decide(pos_point, neg_point, sentiment['polarity'], sentiment['polarity_confidence'])
 
-            if pos_point > neg_point:
-                return (1, pos_point/(pos_point + neg_point))
-            if neg_point > pos_point:
-                return (0, neg_point / (pos_point + neg_point))
 
-            return (-1, 1)
 
         except Exception, e:
             raise e
