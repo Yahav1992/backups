d6466bbd304b76d85c4481089fcd6b1d5dbd6083
Ariel <akarpilovsy@gmail.com>
2017-08-18 03:13:05 +0300

    Creating data to be parsed

--- Manager.py
+++ Manager.py
@@ -6,42 +6,55 @@ import json
 import os
 
 
-symbList=["ibm","viix"]
-for sym in symbList:
-    if os.path.exists(r"result.json"):
-        os.remove(r"result.json")
+if os.path.exists("result.json"):
+    os.remove("result.json")
 
-    process = CrawlerProcess({
+process = CrawlerProcess({
     'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
     'FEED_FORMAT': 'json',
     'FEED_URI': 'result.json'
-    })
+})
 
+symbList = ["ibm", "viix", "aapl"]
+for sym in symbList:
     spider = NasSpider(sym)
     process.crawl(spider, [sym])
-    process.start(stop_after_crawl=False)
-    #
-    # bot = StockNasBot()
-    #
-    # with open('result.json') as data_file:
-    #     data = json.load(data_file)
-    #     for link in data:
-    #         print link
-    #         url = link["link"]
-    #         res = None
-    #         while res is None:
-    #             try:
-    #                 c = Processor()
-    #                 res = c.process(url)
-    #                 if res is None:
-    #                     raise Exception()
-    #
-    #                 if res[0] == 1:
-    #                     #bot.sendmessage('you should buy Stock VIIX  {}'.format(url))
-    #                     print "You should buy %s" % sym
-    #                 print res
-    #
-    #             except Exception, e:
-    #                 if "Too Many " in e.content:
-    #                     print "Renew user"
-    #                 res = None
+process.start()
+
+with open('result.json', 'r+') as datafile:
+    lines = datafile.readlines()
+    datafile.seek(0)
+    datafile.truncate()
+    for line in lines:
+        if "][" in line:
+            line = line.replace("][", "],[")
+        elif "[" in line:
+            line = line.replace("[", "[[")
+        elif "]" in line:
+            line = line.replace("]", "]]")
+        datafile.write(line)
+
+bot = StockNasBot()
+
+with open('result.json') as data_file:
+    data = json.load(data_file)
+    for link in data:
+        print link
+        url = link["link"]
+        res = None
+        while res is None:
+            try:
+                c = Processor()
+                res = c.process(url)
+                if res is None:
+                    raise Exception()
+
+                if res[0] == 1:
+                    #bot.sendmessage('you should buy Stock VIIX  {}'.format(url))
+                    print "You should buy %s" % sym
+                print res
+
+            except Exception, e:
+                if "Too Many " in e.content:
+                    print "Renew user"
+                res = None
